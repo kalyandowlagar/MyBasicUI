f30e9a494b04d9effb7ce29b0e71aa1dd0d44eaf
kalyandowlagar <47981299+kalyandowlagar@users.noreply.github.com>
2020-11-26 21:35:00 +0530

    db set up
    Signed-off-by: kalyandowlagar <47981299+kalyandowlagar@users.noreply.github.com>

--- scripts/signup-page.py
+++ scripts/signup-page.py
@@ -1,10 +1,35 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='127.0.0.1',
+                                         database='motogp',
+                                         user='root',
+                                         password='mysqlpwd@1')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
+
 i = True
 def displayMenu():
     global i, firstname
     while i:
-        firstname = input("First Name :")
+        firstname = input("First Name: ")
+        lastname = input("last Name: ")
         i = False
-    print(f"Hello {firstname}! welcome")
+    print(f"Hello {firstname} {lastname}! welcome")
 
 
 displayMenu()
