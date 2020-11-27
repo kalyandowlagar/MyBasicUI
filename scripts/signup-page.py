bb4df89c76c9007cd9a3b009f96a7202bed67495
kalyandowlagar <47981299+kalyandowlagar@users.noreply.github.com>
2020-11-27 18:44:08 +0530

    db update query
    Signed-off-by: kalyandowlagar <47981299+kalyandowlagar@users.noreply.github.com>

--- scripts/signup-page.py
+++ scripts/signup-page.py
@@ -6,34 +6,31 @@ connection = mysql.connector.connect(host='localhost',
                                          user='root',
                                          password='mysqlpwd@1')
 
-#mycursor = connection.cursor()
-
-#mycursor.execute("SELECT firstname FROM motogp")
-
-#myresult = mycursor.fetchall()
-
-
-i = True
-flag ="N"
 def displayMenu():
-    global i, username
+    global i, username,lastname,email
+    i = True
     while i:
         username = input("First Name: ")
         mycursor = connection.cursor()
-
         mycursor.execute("SELECT firstname FROM motogp")
-
-        myresult = mycursor.fetchall()
-        for x in myresult:
-            if (username == x()):
+        userlist =[item[0] for item in mycursor.fetchall()]
+        flag = "N"
+        for x in userlist:
+            if (x.lower() == username.lower()):
                 flag="Y"
-        if (flag == "N"):
-            lastname = input("last Name: ")
-        else:
+            if (flag == "N"):
+                lastname = input("last Name: ")
+                email = input("enter your email : ")
+                print(f"Hello {username} {lastname}! welcome")
+                newUser()
+        if (flag == "Y"):
             print("User already exist ")
-        i = False
-    print(f"Hello {firstname} {lastname}! welcome")
+            i = False
 
-
+def newUser():
+    mycursor = connection.cursor()
+    sqlQuery = "insert into motogp(firstname,lastname,email) values (%s,%s,%s)"
+    mycursor.execute(sqlQuery,(username,lastname,email))
+    connection.commit()
 
 displayMenu()
